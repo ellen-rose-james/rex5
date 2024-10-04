3dc16c8b4d0739990c060c1c80f8fc971c5e88d2
Dhananjay R <dhananjayrjs1605@gmail.com>
2024-10-04 23:01:45 +0530

    Revert "Merge branch 'main' of https://github.com/ellen-rose-james/rex5"
    This reverts commit 4e3769f45b737872e3481dae63f38cf3b350a646, reversing
    changes made to a6f69a397c3e7fbb39c6d46e07ee97d5a62801ed.

--- assets/js/events.js
+++
@@ -1,6 +0,0 @@
-// let getTcktBtn = document.querySelector('box box3')
-
-// getTcktBtn.addEventListener("hover" , ()=>
-// {
-//     document.getElementById('arrow-icon').style.visibility='visible';
-// })
