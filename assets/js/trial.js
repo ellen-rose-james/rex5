eb039f72eff9440f89a6b5b90d8d68d19bf846f1
ellen-rose-james <ellenrj03@gmail.com>
2024-12-28 13:50:45 +0530

    final updates

--- assets/js/trial.js
+++ assets/js/trial.js
@@ -1,53 +1,9 @@
-/* -- Carousel Navigation -- */
+// Show popup as soon as the page is visited
+window.onload = function() {
+  document.getElementById("popup").style.display = "flex";
+};
 
-let activeIndex = 0;
-
-const slides = document.getElementsByTagName("article");
-
-const handleLeftClick = () => {
-  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
-  
-  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
-        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
-        
-  currentSlide.dataset.status = "after";
-  
-  nextSlide.dataset.status = "becoming-active-from-before";
-  
-  setTimeout(() => {
-    nextSlide.dataset.status = "active";
-    activeIndex = nextIndex;
-  });
-}
-
-const handleRightClick = () => {
-  const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
-  
-  const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
-        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
-  
-  currentSlide.dataset.status = "before";
-  
-  nextSlide.dataset.status = "becoming-active-from-after";
-  
-  setTimeout(() => {
-    nextSlide.dataset.status = "active";
-    activeIndex = nextIndex;
-  });
-}
-
-/* -- Mobile Nav Toggle -- */
-
-const nav = document.querySelector("nav");
-
-const handleNavToggle = () => {  
-  nav.dataset.transitionable = "true";
-  
-  nav.dataset.toggled = nav.dataset.toggled === "true" ? "false" : "true";
-}
-
-window.matchMedia("(max-width: 800px)").onchange = e => {
-  nav.dataset.transitionable = "false";
-
-  nav.dataset.toggled = "false";
+// Close popup when clicking on the close button
+document.getElementById("closePopup").onclick = function() {
+  document.getElementById("popup").style.display = "none";
 };
